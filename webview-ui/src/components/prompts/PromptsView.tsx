b66e4ff2bd26b3e22459054cd96566b3fe181a2d
Matt Rubens <mrubens@users.noreply.github.com>
2025-04-05 14:11:13 -0400

    Update links

--- webview-ui/src/components/prompts/PromptsView.tsx
+++ webview-ui/src/components/prompts/PromptsView.tsx
@@ -792,7 +792,7 @@ const PromptsView = ({ onDone }: PromptsViewProps) => {
 												// Open or create an empty file
 												vscode.postMessage({
 													type: "openFile",
-													text: `./.clinerules-${currentMode.slug}`,
+													text: `./.roorules-${currentMode.slug}`,
 													values: {
 														create: true,
 														content: "",
@@ -928,7 +928,7 @@ const PromptsView = ({ onDone }: PromptsViewProps) => {
 										onClick={() =>
 											vscode.postMessage({
 												type: "openFile",
-												text: "./.clinerules",
+												text: "./.roorules",
 												values: {
 													create: true,
 													content: "",
